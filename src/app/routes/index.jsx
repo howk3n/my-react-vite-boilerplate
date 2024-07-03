698f13d8eead1a2d29bd4d40315d22e1c112eb9d
eriukiyo <dusancvijic1906@gmail.com>
2024-07-04 00:05:36 +0200

    jsdoc annotations added

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
+/**
+ * Uses react-router-dom.createBrowserRouter() to create routes setup
+ * @returns react-router-dom Router object
+ */
 export const createRouter = () => {
   return createBrowserRouter([
     {
--- src/utils/dateTime.js
+++ src/utils/dateTime.js
@@ -1,3 +1,8 @@
+/**
+ * Some default date formatting
+ * @param {string} dateStr - The date string to format
+ * @returns {Date} Formatted date
+ */
 export function formatDate(dateStr) {
   return new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -7,6 +12,11 @@ export function formatDate(dateStr) {
   }).format(new Date(dateStr));
 }
 
+/**
+ * Calculates minutes left between now and dateStr
+ * @param {string} dateStr - The date to compare with
+ * @returns {number} Minutes left between now and dateStr
+ */
 export function calcMinutesLeft(dateStr) {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
